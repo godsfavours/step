cb13dca4ce389b6bc43f1e8034417e9a447e0e2a
Godsfavour <g5simon@stanford.edu>
2020-06-26 10:54:31 -0500

    create blueprint for about me page--to be finished later

--- portfolio/src/main/webapp/page/about/script.js
+++ portfolio/src/main/webapp/page/about/script.js
@@ -13,3 +13,23 @@
 // limitations under the License.
 
 
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide imgages');
+
+// Buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+//Counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+// button listeners
+
+nextBtn.addEventListener('click', ()=> {
+    carouselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+    console.log(counter);
+})
