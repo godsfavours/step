c7279f806c95d810a82f2f64991b3e7cad323045
Godsfavour <g5simon@stanford.edu>
2020-06-30 13:25:06 +0000

    add quotes to webpage using java servlet

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,5 +12,31 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+function getQuote() {
+  console.log('Fetching a random quote.');
 
+  // wait for response
+  const responsePromise = fetch('/data');
 
+  // pass the response to handleResponse()
+  responsePromise.then(handleResponse);
+}
+
+function handleResponse(response) {
+  console.log('Handling the response.');
+
+  // wait for/get text promise from response
+  const textPromise = response.text();
+
+  // if ready, pass along to addQuoteToDom()
+  textPromise.then(addQuoteToDom);
+}
+
+function addQuoteToDom(quote) {
+  console.log('Adding quote to dom: ' + quote);
+
+  const quoteContainer = document.getElementById('quote-container');
+  quoteContainer.innerText = quote;
+
+  console.log('Added quote to dom');
+}
