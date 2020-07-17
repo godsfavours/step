4285364471a9eae579e8a0bb7c11c894b7f62d4b
Godsfavour <g5simon@stanford.edu>
2020-07-17 19:48:52 +0000

    working meeting query

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,126 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+
+
 
 public final class FindMeetingQuery {
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
-  }
+    private ArrayList<TimeRange> availableTimes = new ArrayList<>();
+
+    public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+        availableTimes.clear();
+
+        // If the request is too long, return empty ArrayList
+        if (request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
+            return availableTimes;
+        }
+
+        /* Any time is available during the start. Time ranges will 
+            be dynamically added and removed from the availableTimes 
+            array as the program loops through the events */
+        availableTimes.add(TimeRange.WHOLE_DAY);
+        
+        // If the set of attendees is empty, the entire day is available
+        if (request.getAttendees().isEmpty()) {
+            return availableTimes;
+        }
+
+        // Loop through the attendees in MeetingRequest to find events that they're in
+        Collection<String> attendees = request.getAttendees();
+        Set<Event> eventsSet = new HashSet<Event>(events);
+        for (String attendee : attendees) {
+            System.out.println("Attendee: " + attendees);
+            for (Event event : eventsSet) {
+                Collection<String> eventAttendees = event.getAttendees();
+                if (eventAttendees.contains(attendee)) {
+                    System.out.println("Event attendees: " + eventAttendees.toString());
+                    //eventsSet.remove(event);
+                    TimeRange timeRange = event.getWhen();
+                    System.out.println("Event when: " + timeRange.toString());
+                    blockTimeRange(timeRange, request.getDuration());
+                    System.out.println("New available times: " + availableTimes.toString());
+                  
+                }
+                
+            }
+
+        }
+        
+
+        
+        return availableTimes;
+    }
+
+    // Binary search function to find the TimeRange object to split
+    public void blockTimeRange(TimeRange blockedTimeRange, long meetingDuration) {
+        System.out.println("Blocking time... ");
+        for (int i = 0; i < availableTimes.size(); i++) {
+            TimeRange timeRange = availableTimes.get(i);
+            if (timeRange.overlaps(blockedTimeRange)) {
+                if (blockedTimeRange.contains(timeRange)) {
+                    availableTimes.remove(timeRange);
+                    return;
+                }
+                /*
+                             |--blocked--|
+                          |-----timeRange----|
+                */
+                else if (timeRange.contains(blockedTimeRange)) {
+                    System.out.println(timeRange.toString() + " contains " + blockedTimeRange.toString());
+                    if (blockedTimeRange.start() != timeRange.start()) {
+                        TimeRange newTimeRange1 = TimeRange.fromStartEnd(timeRange.start(), 
+                                                blockedTimeRange.start(), false);
+                        if (newTimeRange1.duration() >= meetingDuration) {
+                            availableTimes.add(i, newTimeRange1);
+                        }
+                    }
+                    if (blockedTimeRange.end() != timeRange.end()) {
+                         TimeRange newTimeRange2 = TimeRange.fromStartEnd(blockedTimeRange.end(), 
+                                                timeRange.end(), false);
+                        if (newTimeRange2.duration() >= meetingDuration) {
+                            availableTimes.add(i + 1, newTimeRange2);
+                        }
+                    }    
+                    availableTimes.remove(timeRange);
+                    return;
+
+                }
+                /*
+                            |--blocked--|
+                    |--timeRange--|
+                */
+                else if (blockedTimeRange.end() > timeRange.end()) {
+                    System.out.println(blockedTimeRange.toString() + " overlaps end of " + timeRange.toString());
+                    TimeRange newTimeRange = TimeRange.fromStartEnd(timeRange.start(), 
+                                                blockedTimeRange.start(), false);
+                    if (newTimeRange.duration() >= meetingDuration) {
+                        availableTimes.add(i, newTimeRange);
+                    }
+                    availableTimes.remove(timeRange);
+                    return;
+                }
+                /*
+                    |--blocked--|
+                          |--timeRange--|
+                */
+                else if (blockedTimeRange.start() < timeRange.start()) {
+                    System.out.println(blockedTimeRange.toString() + " overlaps front of " + timeRange.toString());
+                    TimeRange newTimeRange = TimeRange.fromStartEnd(blockedTimeRange.end(), 
+                                                timeRange.end(), false);
+                    if (newTimeRange.duration() >= meetingDuration) {
+                        availableTimes.add(i, newTimeRange);
+                    }
+                    availableTimes.remove(timeRange);
+                    return;
+                }
+            }
+        }
+    }
 }
+
+
+
+
--- walkthroughs/week-5-tdd/project/src/test/java/com/google/sps/FindMeetingQueryTest.java
+++ walkthroughs/week-5-tdd/project/src/test/java/com/google/sps/FindMeetingQueryTest.java
@@ -22,6 +22,7 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Ignore;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
